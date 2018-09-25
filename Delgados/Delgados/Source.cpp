d37441290e899c441f67c5b67363dfdc3df7df97
Joseph Chalupa <grammerking113@gmail.com>
2018-09-25 10:26:37 -0500

    Update

--- Delgados/Delgados/Source.cpp
+++ Delgados/Delgados/Source.cpp
@@ -1,36 +1,54 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
-	int wantTacos;
-	int wantBurritos;
-	int wantChipsAndSalsa;
-	int wantYellowRice;
-	int wantMiniTacos;
-	double priceTacos = 3.00;
-	double priceBurritos = 3.50;
-	double priceChipsAndSalsa = 2.50;
-	double priceYellowRice = 1.50;
-	double priceMiniTacos = 1.60;
-	double total;
-	double totalAndTax;
+	string choice;
+	double wantTacos = 0;
+	double wantBurritos = 0;
+	double wantChipsAndSalsa = 0;
+	double wantYellowRice = 0;
+	double wantMiniTacos = 0;
 
-	cout << "How many tacos would you like?";
-	cin >> wantTacos;
-	cout << "How Chips and salsa would you like?";
-	cin >> wantChipsAndSalsa;
-	cout << "How many burritos would you like?";
-	cin >> wantBurritos;
-	cout << "How many mini tacos would you like?";
-	cin >> wantMiniTacos;
-	cout << "How much yellow rice would you like?";
-	cin >> wantYellowRice;
+	do {
+		cout << "What would you like to order?\nTacos\nBurritos\nChips\nMini Tacos\nYellow Rice\n>>  ";
+		cin >> choice;
+		if (choice == "Tacos" || "tacos")
+		{
+			cout << "How many tacos would you like? >> ";
+			cin >> wantTacos;
+		}
+		else if (choice == "chips" || "Chips")
+		{
+			cout << "How much chips and salsa would you like? >> ";
+			cin >> wantChipsAndSalsa;
+		}
+		else if (choice == "burritos" || "Burritos")
+		{
+			cout << "How many burritos would you like? >> ";
+			cin >> wantBurritos;
+		}
+		else if (choice == "Mini Tacos" || "mini tacos" || "Mini tacos" || "mini Tacos")
+		{
+			cout << "How many mini tacos would you like? >> ";
+			cin >> wantMiniTacos;
+		}
+		else if (choice == "Yellow Rice" || "yellow rice" || "Yellow rice" || "yellow Rice")
+		{
+			cout << "How much rice would you like? >> ";
+			cin >> wantBurritos;
+		}
+	} while (choice == "Yellow Rice" || "yellow rice" || "Yellow rice" || "yellow Rice" || "Mini Tacos" || "mini tacos" || "Mini tacos" || "mini Tacos" || "burritos" || "Burritos" || "Tacos" || "tacos" || "chips" || "Chips");
+	
+	total(wantTacos, wantBurritos, wantYellowRice, wantChipsAndSalsa, wantMiniTacos);
 
-	total = (wantTacos * priceTacos) + (wantBurritos * priceBurritos) + (wantYellowRice * priceYellowRice) + (wantChipsAndSalsa * priceChipsAndSalsa) + (wantMiniTacos * priceMiniTacos);
-	totalAndTax = total + (total * .07);
+	system("pause");
+	return 0;
+}
 
-	cout << "Your Total without tax is $" << total << " and with tax it is $" << totalAndTax << endl;
+int truck()
+{
 	cout << "                    XXXXXX" << endl;
 	cout << "                XXXXXXXXXXXXXXX" << endl;
 	cout << "              XXXXXXXXXXXXXXXXXXX" << endl;
@@ -45,6 +63,21 @@ int main()
 	cout << "          XXXXXX               XXXXXXXX          " << endl;
 	cout << "           XXXX                  XXXX             " << endl;
 
-	system("pause");
 	return 0;
+}
+
+int total(int wantTacos, int wantBurritos, int wantYellowRice, int wantChipsAndSalsa, int wantMiniTacos) {
+	double totalAndTax;
+	double total;
+	double priceTacos = 3.00;
+	double priceBurritos = 3.50;
+	double priceChipsAndSalsa = 2.50;
+	double priceYellowRice = 1.50;
+	double priceMiniTacos = 1.60;
+	total = (wantTacos * priceTacos) + (wantBurritos * priceBurritos) + (wantYellowRice * priceYellowRice) + (wantChipsAndSalsa * priceChipsAndSalsa) + (wantMiniTacos * priceMiniTacos);
+	totalAndTax = total + (total * .07);
+
+
+	return totalAndTax;
+
 }
--- Eggs/Eggs/Source.cpp
+++ Eggs/Eggs/Source.cpp
@@ -4,20 +4,22 @@ using namespace std;
 
 int main()
 {
+	int eggsRemain = 100;
 	int eggsWant;
 	int eggsDozenWant;
 	double eggPrice1 = 0.25;
 	double eggPrice2 = 2.00;
 	double eggTotal;
 
-	cout << "There are 1 million eggs left. How many dozens of eggs would you like to buy?";
+	cout << "There are " << eggsRemain << " left. How many dozens of eggs would you like to buy?";
 	cin >> eggsDozenWant;
 	cout << "How many seperate of eggs would you like to buy?";
 	cin >> eggsWant;
 
 	eggTotal = (eggsDozenWant * eggPrice2) + (eggsWant * eggPrice1);
+	eggsRemain = eggsRemain - ((eggsDozenWant + eggsWant);
 
-	cout << "Your total will be $" << eggTotal << ". Thank you." << endl;
+	cout << "Your total will be $" << eggTotal << " and ther are " << eggsRemain << " Thank you." << endl;
 	
 	system("pause");
 	return 0;
